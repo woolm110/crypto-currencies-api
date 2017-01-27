1b99705c9f555bfca48bc30c8635d18c11f24575
Max Woollard <max.woollard@ostmodern.co.uk>
2017-01-27 15:42:21 +0000

    Remove unecessary coins

--- utils/prices.js
+++ utils/prices.js
@@ -41,18 +41,6 @@ const cryptoCurrencyMap = {
     pair: 'ETH-USD',
     intermediateCurrency: null
   },
-  LTC: {
-    exchange: 'gdax',
-    hasNativeCurrency: true,
-    pair: 'LTC-USD',
-    intermediateCurrency: null
-  },
-  REP: {
-    exchange: 'poloniex',
-    hasNativeCurrency: true,
-    pair: 'USDT_REP',
-    intermediateCurrency: null
-  },
   ZEC: {
     exchange: 'poloniex',
     hasNativeCurrency: true,
@@ -65,88 +53,16 @@ const cryptoCurrencyMap = {
     pair: 'USDT_XMR',
     intermediateCurrency: null
   },
-  ETC: {
-    exchange: 'poloniex',
-    hasNativeCurrency: true,
-    pair: 'USDT_ETC',
-    intermediateCurrency: null
-  },
-  XRP: {
-    exchange: 'poloniex',
-    hasNativeCurrency: true,
-    pair: 'USDT_XRP',
-    intermediateCurrency: null
-  },
   DASH: {
     exchange: 'poloniex',
     hasNativeCurrency: true,
     pair: 'USDT_DASH',
     intermediateCurrency: null
   },
-  STR: {
-    exchange: 'poloniex',
-    hasNativeCurrency: true,
-    pair: 'USDT_STR',
-    intermediateCurrency: null
-  },
-  LSK: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_LSK',
-    intermediateCurrency: 'BTC'
-  },
   MAID: {
     exchange: 'poloniex',
     hasNativeCurrency: false,
     pair: 'BTC_MAID',
-    intermediateCurrency: 'BTC'
-  },
-  FCT: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_FCT',
-    intermediateCurrency: 'BTC'
-  },
-  XEM: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_XEM',
-    intermediateCurrency: 'BTC'
-  },
-  STEEM: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'ETH_STEEM',
-    intermediateCurrency: 'ETH'
-  },
-  DOGE: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_DOGE',
-    intermediateCurrency: 'BTC'
-  },
-  SDC: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_SDC',
-    intermediateCurrency: 'BTC'
-  },
-  BTS: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_BTS',
-    intermediateCurrency: 'BTC'
-  },
-  GAME: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_GAME',
-    intermediateCurrency: 'BTC'
-  },
-  ARDR: {
-    exchange: 'poloniex',
-    hasNativeCurrency: false,
-    pair: 'BTC_ARDR',
     intermediateCurrency: 'BTC'
   },
   PASC: {
